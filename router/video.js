1e967fc65e35d6dde786875bf9a8274fb10744ec
ZiWei Rao <r3033595562@gmail.com>
2021-02-03 14:24:12 -0500

    Set up all the middlewares, and request router for uploading videos

--- router/video.js
+++ router/video.js
@@ -1,8 +1,42 @@
 const express = require('express');
+const path = require('path');
+const fs = require('fs');
+const awaitWriteStream = require('await-stream-ready').write;
+const sendToWormhole = require('stream-wormhole');
+const Busboy = require('busboy');
 const router = express.Router();
 
-router.get('/index', (req, res) => {
+router.get('/', (req, res) => {
     res.send('Hi api!');
+});
+
+router.post('/upload_file', async (req, res) => {
+    const busboy = new Busboy({ headers: req.headers });
+    busboy.on('file', async (fieldname, file, filename, encoding, mimetype) => {
+        //console.log('File [' + fieldname + ']: filename: ' + filename + ', encoding: ' + encoding + ', mimetype: ' + mimetype);
+        // 获取文件流
+        const stream = file;
+        // 定义文件名
+        const upload_name = Date.now() + path.extname(filename).toLocaleLowerCase()
+        // 目标文件
+        const target = path.join('public', upload_name);
+        //
+        const writeStream = fs.createWriteStream(target);
+        try {
+            console.log('Start upload');
+            //异步把文件流 写入
+            await awaitWriteStream(stream.pipe(writeStream));
+            console.log('Upload done!');
+        } catch (err) {
+            //如果出现错误，关闭管道
+            await sendToWormhole(stream);
+            // 自定义方法
+            res.json({ success: false });
+        }
+        res.json({ success: true });
+    });
+    req.pipe(busboy);
+
 });
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,10 +1,23 @@
 const express = require('express');
 const app = express();
 const config = require('./config');
+const path = require('path');
 const PORT = config.DEFAULT_PORT;
 
 // Load all the router
 const video = require('./router/video');
+
+// Load all the middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use('/public', express.static(path.join(__dirname, 'public')));
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", req.headers.origin);
+    res.header("Access-Control-Allow-Credentials", true);
+    res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT,DELETE");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+    next();
+});
 
 // Set all router to app
 app.use('/video', video);
