2c7db5a4063a740d5ad2c8dfdb1b36d364622793
Ye Qi <qi.ye@live.com>
2021-03-01 21:41:33 -0500

    upload to s3

---
+++ private/keys.js
@@ -0,0 +1,5 @@
+module.exports = {
+  AWS_AK: "AKIA3NMN4AKB27RHNDUV",
+  AWS_SK: "TaESd1ZYwEh5/ap/r2y7toqOi8nBfdndH8ROstUA",
+  BUCKET_NAME: "survey-videos-mohawk-dev",
+};
--- router/video.js
+++ router/video.js
@@ -1,43 +1,44 @@
-const express = require('express');
-const path = require('path');
-const fs = require('fs');
-const awaitWriteStream = require('await-stream-ready').write;
-const sendToWormhole = require('stream-wormhole');
-const Busboy = require('busboy');
+const express = require("express");
+const path = require("path");
+// const fs = require("fs");
+// const awaitWriteStream = require("await-stream-ready").write;
+const sendToWormhole = require("stream-wormhole");
+const Busboy = require("busboy");
+const uploadFile = require("../utils/upload_s3");
+
 const router = express.Router();
 
-router.get('/', (req, res) => {
-    res.send('Hi api!');
+router.get("/", (req, res) => {
+  res.send("Hi api!");
 });
 
-router.post('/upload_file', async (req, res) => {
-    const busboy = new Busboy({ headers: req.headers });
-    busboy.on('file', async (fieldname, file, filename, encoding, mimetype) => {
-        try {
-            //console.log('File [' + fieldname + ']: filename: ' + filename + ', encoding: ' + encoding + ', mimetype: ' + mimetype);
-            // 获取文件流
-            const stream = file;
-            // 定义文件名
-            const upload_name = Date.now() + path.extname(filename).toLocaleLowerCase()
-            // 目标文件
-            const target = path.join('public', upload_name);
-            //
-            const writeStream = fs.createWriteStream(target);
+router.post("/upload_file", async (req, res) => {
+  const busboy = new Busboy({ headers: req.headers });
+  busboy.on("file", async (fieldname, file, filename, encoding, mimetype) => {
+    try {
+      //console.log('File [' + fieldname + ']: filename: ' + filename + ', encoding: ' + encoding + ', mimetype: ' + mimetype);
+      // Define path
+      const upload_name =
+        Date.now() + path.extname(filename).toLocaleLowerCase();
+      //   const target = path.join("public", upload_name);
+      // set writer
+      //   const writeStream = fs.createWriteStream(target);
 
-            console.log('Start upload');
-            //异步把文件流 写入
-            await awaitWriteStream(stream.pipe(writeStream));
-            console.log('Upload done!');
-        } catch (err) {
-            //如果出现错误，关闭管道
-            await sendToWormhole(stream);
-            // 自定义方法
-            res.json({ success: false });
-        }
-        res.json({ success: true });
-    });
-    req.pipe(busboy);
+      console.log("Start upload");
+      // async write stream
+      //   await awaitWriteStream(file.pipe(writeStream));
+      uploadFile(file, upload_name);
 
+      console.log("Upload done!");
+    } catch (err) {
+      // close channel when error is caught
+      //   await sendToWormhole(stream);
+      // set error response
+      res.json({ success: false });
+    }
+    res.json({ success: true });
+  });
+  req.pipe(busboy);
 });
 
 module.exports = router;
