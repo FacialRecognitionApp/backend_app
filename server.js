e01429a5cc71f377b1b68d25f3774efb7d3e7dba
Ye Qi <qi.ye@live.com>
2021-03-13 12:58:40 -0500

    allow cors

--- server.js
+++ server.js
@@ -1,31 +1,45 @@
 // Load all packages
-const express = require('express');
-const app = express();
-const config = require('./config');
-const path = require('path');
-const PORT = config.DEFAULT_PORT;
+const express = require("express");
+const cors = require("cors");
+const config = require("./config");
+const path = require("path");
 
 // Load all the router
-const video = require('./router/video');
-const survey = require('./router/survey');
-const user = require('./router/user');
+const video = require("./router/video");
+const survey = require("./router/survey");
+const user = require("./router/user");
+const PORT = config.DEFAULT_PORT;
+
+const app = express();
 
 // Load all the middleware
+app.use(
+  cors({
+    origin: "*",
+    optionsSuccessStatus: 200, // For legacy browser support
+  })
+);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use('/public', express.static(path.join(__dirname, 'public')));
+app.use("/public", express.static(path.join(__dirname, "public")));
 app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", req.headers.origin);
-    res.header("Access-Control-Allow-Credentials", true);
-    res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT,DELETE");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
-    next();
+  res.header("Access-Control-Allow-Origin", req.headers.origin);
+  res.header("Access-Control-Allow-Credentials", true);
+  res.header(
+    "Access-Control-Allow-Methods",
+    "GET,HEAD,OPTIONS,POST,PUT,DELETE"
+  );
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
+  next();
 });
 
 // Set all router to app
-app.use('/video', video);
-app.use('/survey', survey);
-app.use('/user', user);
+app.use("/video", video);
+app.use("/survey", survey);
+app.use("/user", user);
 
 // Start Server
 app.listen(PORT, () => console.log(`Server is running on ${PORT}`));
